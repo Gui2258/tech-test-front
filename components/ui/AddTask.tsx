3a721698de4c4719f273d002da4bb9a418de6370
Guido Morffi <guidotele@gmail.com>
2025-01-29 12:38:11 -0500

    (feat) Expand button - Added context provoder for state managment

--- components/ui/AddTask.tsx
+++ components/ui/AddTask.tsx
@@ -1,8 +1,12 @@
 'use client';
 import { Plus } from '@/components/icons/Plus';
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { AddingTask } from './AddingTask';
 
+export const addTask = createContext({
+    showDorp: false,
+    taskText: '',
+});
 export default function AddTask() {
     const [isFocused, setIsFocused] = useState(false);
     const [inputValue, setInputValue] = useState('');
@@ -14,34 +18,38 @@ export default function AddTask() {
 
     return (
         <>
-            <main>
-                <div className="flex flex-col">
-                    <div className="flex">
-                        <Plus />
-                        <input
-                            className="bg-gray-placeholder focus:outline-none"
-                            placeholder="Type to add new Task"
-                            value={inputValue}
-                            onChange={(e) => setInputValue(e.target.value)}
-                            onFocus={() => setIsFocused(true)}
-                            onBlur={() => setIsFocused(false)}
-                        />
-                    </div>
+            <addTask.Provider
+                value={{ showDorp: showDropdown, taskText: inputValue }}
+            >
+                <main>
+                    <div className="flex flex-col">
+                        <div className="flex">
+                            <Plus />
+                            <input
+                                className="bg-gray-placeholder focus:outline-none"
+                                placeholder="Type to add new Task"
+                                value={inputValue}
+                                onChange={(e) => setInputValue(e.target.value)}
+                                onFocus={() => setIsFocused(true)}
+                                onBlur={() => setIsFocused(false)}
+                            />
+                        </div>
 
-                    <div
-                        className={`
-                        transform transition-all duration-300 ease-in-out
-                        ${
-                            showDropdown
-                                ? 'opacity-100 translate-y-2 h-32'
-                                : 'opacity-0 -translate-y-4 h-0'
-                        }
-                    `}
-                    >
-                        <AddingTask />
+                        <div
+                            className={`
+                            transform transition-all duration-300 ease-in-out
+                            ${
+                                showDropdown
+                                    ? 'opacity-100 translate-y-2 h-32'
+                                    : 'opacity-0 -translate-y-4 h-0'
+                            }
+                            `}
+                        >
+                            <AddingTask />
+                        </div>
                     </div>
-                </div>
-            </main>
+                </main>
+            </addTask.Provider>
         </>
     );
 }
--- components/ui/AddingTask.tsx
+++ components/ui/AddingTask.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ExpandButton } from './buttons/ExpandButton';
+import { addTask } from './AddTask';
 
 export const AddingTask: React.FunctionComponent = () => {
+    const showDorp = useContext(addTask);
     return (
         <>
             <div className="bg-white p-4 rounded shadow-lg">
-                <ExpandButton isDisabled={true} />
+                <ExpandButton
+                    isDisabled={
+                        showDorp.showDorp && showDorp.taskText.length == 0
+                    }
+                />
             </div>
         </>
     );
