8b546aae15f499440ff30bb5c6828f3a8b5cdbdb
Guido Morffi <guidotele@gmail.com>
2025-01-29 09:53:41 -0500

    (feat) Homa Page - Added transition on input focus

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
+'use client';
 import { Plus } from '@/components/icons/Plus';
+import { useState } from 'react';
 
 export default function Home() {
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
         <>
             <main>
-                <Plus />
-                Hola mundo
+                <div className="flex flex-col">
+                    <div className="flex">
+                        <Plus />
+                        <input
+                            className="bg-gray-placeholder"
+                            placeholder="Type to add new Task"
+                            onFocus={() => setIsFocused(true)}
+                            onBlur={() => setIsFocused(false)}
+                        />
+                    </div>
+
+                    <div
+                        className={`
+                        transform transition-all duration-300 ease-in-out
+                        ${
+                            isFocused
+                                ? 'opacity-100 translate-y-2 h-32'
+                                : 'opacity-0 -translate-y-4 h-0'
+                        }
+                    `}
+                    >
+                        <div className="bg-white p-4 rounded shadow-lg">
+                            {/* Your dropdown content here */}
+                            <p>Additional options or content</p>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     );
