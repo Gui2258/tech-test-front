8b2c382bf3594a38fb906895113d387dc5dec0bf
Guido Morffi <guidotele@gmail.com>
2025-01-30 09:41:41 -0500

    (feat) Task component - Implemented loadin and error handling logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AddTask from '@/components/ui/AddTask';
+import { Tasks } from '@/components/ui/Tasks';
 
 export default function Home() {
     return (
         <>
             <AddTask />
+            <Tasks />
         </>
     );
 }
