87f9fe5a41b6fc74978644c9c8cb8ad4739877ec
Guido Morffi <guidotele@gmail.com>
2025-01-30 19:36:12 -0500

    (feat) Save-Delete button: Added logic for delete/save

--- components/ui/buttons/EliminateSaveButton.tsx
+++ components/ui/buttons/EliminateSaveButton.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import { serverFetcher } from '@/components/api/serverFetcher';
+import React, { useContext, useEffect, useState } from 'react';
+import { addTask } from '../AddTask';
+import { Itasks } from '@/utils/types';
 
 interface IEliminateSaveButtonProps {
     value: string;
@@ -8,15 +11,54 @@ interface IEliminateSaveButtonProps {
 
 export const EliminateSaveButton: React.FunctionComponent<
     IEliminateSaveButtonProps
-> = ({ original, value }) => {
+> = ({ original, value, id }) => {
     const [isEditing, setisEditing] = useState(false);
+    const { getTasks } = useContext(addTask);
+    const deleteTasks = async () => {
+        try {
+            await serverFetcher<Itasks>(`/tasks/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            getTasks();
+        } catch (error) {
+            console.error('Error al crear tarea');
+            console.error(error);
+        }
+    };
+    const updateTasks = async () => {
+        try {
+            await serverFetcher<Itasks>(`/tasks/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    content: value,
+                }),
+            });
+            getTasks();
+        } catch (error) {
+            console.error('Error al crear tarea');
+            console.error(error);
+        }
+    };
     useEffect(() => {
         setisEditing(original !== value);
     }, [original, value]);
 
     return (
         <>
-            <button>{isEditing ? 'Save' : 'Eliminar'}</button>
+            <button
+                onClick={() => {
+                    if (isEditing) updateTasks();
+                    else deleteTasks();
+                }}
+            >
+                {isEditing ? 'Save' : 'Eliminar'}
+            </button>
         </>
     );
 };
