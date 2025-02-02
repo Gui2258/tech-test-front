47f7bd6b0dc8de5a7c7556e2a29837a9c4c7de08
Guido Morffi <guidotele@gmail.com>
2025-02-02 17:53:58 -0500

    (assets) Added mesagge and trash icons

---
+++ components/ui/buttons/MessageButton.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React, { useContext, useState } from 'react';
+import clsx from 'clsx';
+import { addTask } from '../AddTask';
+import { MessageIcon } from '@/components/icons/MessageIcon';
+
+export const MessageButton: React.FunctionComponent = ({}) => {
+    const [isHover, setisHover] = useState(false);
+    const { taskText, showDorp } = useContext(addTask);
+    const isDisabled = taskText.length === 0 && showDorp;
+
+    return (
+        <>
+            <button
+                className={clsx(
+                    'flex font-bold p-2 xl:py-2 xl:pl-4 xl:pr-6 gap-3  justify-center items-center  h-10 rounded xl:border-solid xl:border-2',
+                    { ' border-[#cfd1d0] hover:bg-gray-200': !isDisabled },
+                    { ' border-[#eaeceb]': isDisabled }
+                )}
+                onMouseMove={() => setisHover(true)}
+                onMouseLeave={() => setisHover(false)}
+            >
+                <MessageIcon
+                    isHover={isHover}
+                    isDisabled={taskText.length == 0 && showDorp}
+                />
+                <span
+                    className={clsx(
+                        'hidden xl:block',
+                        { ' text-[#9296a1] ': !isDisabled },
+                        { ' text-[#d2d5da]': isDisabled }
+                    )}
+                >
+                    Today
+                </span>
+            </button>
+        </>
+    );
+};
