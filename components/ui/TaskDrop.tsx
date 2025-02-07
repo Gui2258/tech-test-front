81a755c2f9181c8438bac084067ad51b55a3948d
Guido Morffi <guidotele@gmail.com>
2025-02-07 08:40:26 -0500

    (styles) Corretced gap for mobile view

--- components/ui/TaskDrop.tsx
+++ components/ui/TaskDrop.tsx
@@ -35,8 +35,8 @@ export const TaskDrop: React.FunctionComponent<ITaskDropProps> = ({
                                     }
                                     `}
             >
-                <div className="bg-white flex p-2 rounded shadow-lg gap-8">
-                    <div id="action buttons" className="flex gap-8">
+                <div className="bg-white flex p-2 rounded shadow-lg gap-3 sm:gap-8 ">
+                    <div id="action buttons" className="flex gap-3 sm:gap-8 ">
                         <div>
                             <ExpandButton />
                         </div>
